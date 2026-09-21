3599c05be7cf358cbb209711ca66e960b5a10ef4
syavanadwi8@gmail.com <pacarjenoo200@gmail.com>
2026-09-21 14:58:06 +0700

    first commit

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,16 @@
+import { View, Text, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { MaterialIcons } from "@expo/vector-icons";
+
+export default function HomeScreen({ navigation }: any) {
+  return;
+  <SafeAreaView className="flex-1 bg-white">
+    <View className="flex-row justify-between items-center px-4 pb-8">
+      <Text className="text-[20px]">hai, syavana</Text>
+      <Text></Text>
+    </View>
+    <View>
+      <Image />
+    </View>
+  </SafeAreaView>;
+}
